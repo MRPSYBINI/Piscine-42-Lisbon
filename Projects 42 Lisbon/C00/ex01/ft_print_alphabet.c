e7a8443aa7e94996494fa85d1bbdc5f42dbbf5e4
Leonardo <139896299+MRPSYBINI@users.noreply.github.com>
2023-09-10 16:56:58 +0100

    Add files via upload

---
+++ Projects 42 Lisbon/C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		write(1, &i, 1);
+		i++;
+	}
+}
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
